5dfaf2862928a0d3083189ee743cd92be345d2ac
iLyK <ilyk@ilyk.im>
2026-01-21 15:40:40 -0800

    Update contract title in CWLootBox.sol

--- contracts/CWLootBox.sol
+++ contracts/CWLootBox.sol
@@ -8,7 +8,7 @@ import {ERC1155Tradable} from "./libs/opensea/ERC1155Tradable.sol";
 import {ICWERC1155} from "./ICWERC1155.sol";
 
 /**
- * @title LootBox test
+ * @title LootBox test 221
  * LootBox - a randomized and openable lootbox of ChainWars
  */
 contract CWLootBox is ERC1155Tradable, LootBoxData, RandomNumberConsumer, ReentrancyGuard {
